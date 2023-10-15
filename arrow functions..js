e0db8cbfe5379e1fd09ccff133c6f9906665a73c
Alfina.M <145192885+Alfinanasdeen@users.noreply.github.com>
2023-10-15 19:25:32 +0530

    arrow functions..js

---
+++ arrow functions..js
@@ -0,0 +1,83 @@
+//QS.2.Do the below programs in arrow functions.
+
+//a.Print odd numbers in an array
+
+var y=[1,2,3,4,5,6,7]
+
+var odd=(array)=>{
+   let arr=[];
+   for(let i=0; i<array.length; i++)
+   {
+     if(array[i]%2!==0)
+     {
+      arr.push(array[i]);
+     }
+   }
+   return arr;
+}
+console.log(odd(y));
+
+//b.Convert all the strings to title caps in a string array
+
+var str="my name is alfina";
+let def= (str)=>{
+   str = str.toLowerCase().split(' ');
+   for (var i = 0; i < str.length; i++) {
+     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+   } 
+   return str.join(' ');
+}
+console.log(def(str));
+
+//c.Sum of all numbers in an array
+
+var a=[1,2,3,4,5,6,7,8,9];
+var sum=0;
+let abc=(a)=>
+{
+   for(let i=0; i<a.length; i++)
+   {
+     sum=sum+a[i];
+   }
+   return sum;
+}
+console.log(abc(a));
+
+//d.Return all the prime numbers in an array
+
+let n=34;
+let l=(n)=>
+{
+   for(let i=2; i<=n; i++)
+   {
+      let flag=0;
+      for(let j=2; j<i; j++)
+      {
+         if(i%j==0)
+         {
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+      {
+         console.log(i);
+      }
+   }
+}
+console.log(l(n));
+
+//e.Return all the palindromes in an array
+
+Palindrome = (arr, n) =>
+    {
+        
+        for (let i = 0; i < n; i++)
+        {
+            let ans = isPalindrome(arr[i]);
+            if (ans == false)
+                return false;
+        }
+        return true;
+    }
+
